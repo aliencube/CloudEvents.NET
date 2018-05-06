fcce15dc2d5da98ee26d0d4ee1d204b018558224
Justin Yoo <justin.yoojh@gmail.com>
2018-05-06 23:05:33 +1000

    Add a solution and blank projects

---
+++ test/Aliencube.CloudEvents.Abstractions.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aliencube.CloudEvents.Abstractions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
