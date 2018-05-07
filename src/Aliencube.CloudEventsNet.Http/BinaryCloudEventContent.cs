582c5ad616c5d3aeefd8d1fada94944c3fdaebe7
Justin Yoo <justin.yoojh@gmail.com>
2018-05-07 14:04:21 +1000

    Add CloudEventContent classes

---
+++ src/Aliencube.CloudEventsNet.Http/BinaryCloudEventContent.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Aliencube.CloudEventsNet.Http
+{
+    public class BinaryCloudEventContent : ByteArrayContent
+    {
+        public BinaryCloudEventContent(byte[] content) : base(content)
+        {
+        }
+
+        public BinaryCloudEventContent(byte[] content, int offset, int count) : base(content, offset, count)
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        protected override Task<Stream> CreateContentReadStreamAsync()
+        {
+            return base.CreateContentReadStreamAsync();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            return base.SerializeToStreamAsync(stream, context);
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            return base.TryComputeLength(out length);
+        }
+    }
+}
---
+++ src/Aliencube.CloudEventsNet.Http/StructuredCloudEventContent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Aliencube.CloudEventsNet.Http
+{
+    public class StructuredCloudEventContent : HttpContent
+    {
+        protected StructuredCloudEventContent()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        protected override Task<Stream> CreateContentReadStreamAsync()
+        {
+            return base.CreateContentReadStreamAsync();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
