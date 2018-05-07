1c87b4ce6fe2f0bb95e5114a05b6856c9b1e24d4
Justin Yoo <justin.yoojh@gmail.com>
2018-05-08 03:13:54 +1000

    Add sample API app that sends CloudEvent payload to Azure Event Grid

---
+++ sample/Aliencube.CloudEventsNet.ApiApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aliencube.CloudEventsNet.ApiApp.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
